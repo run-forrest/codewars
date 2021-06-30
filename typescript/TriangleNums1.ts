9a66c7596245eb3332e143eea37faa1905fd937e
forrest <aleremirror@gmail.com>
2021-06-30 13:46:15 +0800

    feature: triangle of consecutive odd

--- typescript/TriangleNums1.ts
+++ typescript/TriangleNums1.ts
@@ -1,4 +1,6 @@
 export function TrangleNums(n: number) {
-    if(n == 1) return 1;
-    return ((n-1)*(n)+1)*n+(n-1)*n;
+    if (n == 1) return 1;
+    // return ((n-1)*(n)+1)*n+(n-1)*n;
+    // return n ** 3;
+    return Math.pow(n, 3);
 }
