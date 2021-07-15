f8a8303b7abf7a0078156993a1460999a7aec635
forrest <aleremirror@gmail.com>
2021-07-15 10:42:44 +0800

    feature: MaximumMultipleOC

---
+++ typescript/MaximumMultiple.ts
@@ -0,0 +1,12 @@
+export function maxMultiple(divisor: number, bound: number) {
+    for (let i = bound; i > 0; i--) {
+        if (i % divisor === 0) {
+            return i;
+        }
+    }
+}
+
+
+export function maxMultiple2(divisor: number, bound: number) {
+        
+    }
