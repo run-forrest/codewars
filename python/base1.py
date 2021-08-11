da725de1a54ff6ae786ec534abe839fc014d9b4e
forrest <aleremirror@gmail.com>
2021-08-11 19:39:58 +0800

    feature: sort

--- python/base1.py
+++ python/base1.py
@@ -4,3 +4,10 @@
 # "-7" --> -7
 def string_to_number(s):
     return int(s)
+
+
+def comp(array1, array2):
+   try:
+        return sorted([i ** 2 for i in array1]) == sorted(array2)
+    except:
+        return False
